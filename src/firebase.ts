16b5fc3588a7c922dd59110c78e62f7143173dab
HilalBoBeng <117025018+HilalBoBeng@users.noreply.github.com>
2025-08-20 22:22:13 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDL-2vFwkMlM__S8Vam_SD1D8pk3G2exs8",
-    authDomain: "ppdb-online-361ef.firebaseapp.com",
-    projectId: "ppdb-online-361ef",
-    storageBucket: "ppdb-online-361ef.firebasestorage.app",
-    messagingSenderId: "559756267963",
-    appId: "1:559756267963:web:bcbb2b114e6c3381a4749e",
-    measurementId: "G-2NEQZ7SRFT"
+  authDomain: "ppdb-online-361ef.firebaseapp.com",
+  databaseURL: " https://ppdb-online-361ef-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "ppdb-online-361ef",
+  storageBucket: "ppdb-online-361ef.firebasestorage.app",
+  messagingSenderId: "559756267963",
+  appId: "1:559756267963:web:bcbb2b114e6c3381a4749e",
+  measurementId: "G-2NEQZ7SRFT"
 }
 
 const app = initializeApp(firebaseConfig)
