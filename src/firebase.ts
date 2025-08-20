0d26022a4b7f70d4d4b5c4ed2cdeae4b93b24ef4
HilalBoBeng <117025018+HilalBoBeng@users.noreply.github.com>
2025-08-20 22:25:50 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,22 @@
-import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDL-2vFwkMlM__S8Vam_SD1D8pk3G2exs8",
   authDomain: "ppdb-online-361ef.firebaseapp.com",
-  databaseURL: " https://ppdb-online-361ef-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  databaseURL: "https://ppdb-online-361ef-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "ppdb-online-361ef",
   storageBucket: "ppdb-online-361ef.firebasestorage.app",
   messagingSenderId: "559756267963",
   appId: "1:559756267963:web:bcbb2b114e6c3381a4749e",
   measurementId: "G-2NEQZ7SRFT"
-}
+};
 
-const app = initializeApp(firebaseConfig)
-export const database = getDatabase(app)
-export const auth = getAuth(app)
-export const firestore = getFirestore(app)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
